4a2a472c9a47152a14ccaeeaec5e6c706c0ad560
kouroshelkaie <kourosh.elkaie@gmail.com>
2021-12-08 15:55:45 +0330

    add banner section

--- scripts/ui.js
+++ scripts/ui.js
@@ -6,10 +6,10 @@ const totalDeath =   document.querySelector("#gd-totaldeath")
 
 // the function that get global data and put them on dom 
 const getGlobalData = (globalData)=>{
-    newConfirmed.innerHTML = globalData.NewConfirmed
-    newDeath.innerHTML =     globalData.NewDeaths
-    totalCases.innerHTML =   globalData.TotalConfirmed
-    totalDeath.innerHTML =   globalData.TotalDeaths
+    newConfirmed.innerHTML = globalData.NewConfirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    newDeath.innerHTML =     globalData.NewDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    totalCases.innerHTML =   globalData.TotalConfirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    totalDeath.innerHTML =   globalData.TotalDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 // the function that get data by country name and put them on dom 
