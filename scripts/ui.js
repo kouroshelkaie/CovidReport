64aa5c8c5145844a0f1e65bbba3d897816b033cd
kouroshelkaie <kourosh.elkaie@gmail.com>
2021-12-09 15:46:48 +0330

    add iran report section

--- scripts/ui.js
+++ scripts/ui.js
@@ -3,6 +3,11 @@ const newConfirmed = document.querySelector("#gd-newconfirmed")
 const newDeath =     document.querySelector("#gd-newdeaths")
 const totalCases =   document.querySelector("#gd-totalcases")
 const totalDeath =   document.querySelector("#gd-totaldeath")
+const irFlag =       document.querySelector(".iran_img")
+irDeath =            document.querySelector(".iran_death_data")
+irCase =             document.querySelector(".iran_case_data")
+
+
 
 // the function that get global data and put them on dom 
 const getGlobalData = (globalData)=>{
@@ -15,4 +20,11 @@ const getGlobalData = (globalData)=>{
 // the function that get data by country name and put them on dom 
 const getCountryData = (countryData)=>{
     console.log(countryData)
+    
+    // put country(iran) data on dom
+    irFlag.setAttribute('src',countryData[95].countryInfo.flag);
+    irDeath.innerHTML = countryData[95].todayDeaths
+    irCase.innerHTML = countryData[95].todayCases
 }
+
+// iran Section
